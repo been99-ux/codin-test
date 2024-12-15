f4bc2dbb41af17e3c2b29ecb81746617864be6ce
been991122 <been99112232@gmail.com>
2024-12-15 18:57:00 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String message = "Let's go !";
+
+        System.out.println("3\n 2\n 1");
+        System.out.println(message);
+
+        }
+    }
