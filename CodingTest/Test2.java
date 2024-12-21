7455124c2d140254743665a042f810d3b4e35bdd
been991122 <been99112232@gmail.com>
2024-12-21 22:43:22 +0900

    Add files via upload

---
+++ CodingTest/Test2.java
@@ -0,0 +1,11 @@
+package CodingTest;
+
+public class Test2 {
+    public static void main(String[] args) {
+        String message = "Let's go !";
+
+        System.out.println("3\n 2\n 1");
+        System.out.println(message);
+
+    }
+}
---
+++ CodingTest/Test4.java
@@ -0,0 +1,4 @@
+package CodingTest;
+
+public class Test4 {
+}
